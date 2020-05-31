f3c64084714f97338ba2da7f7bd880d331c3e66b
Jackseed <vinkho.c@gmail.com>
2020-05-31 16:43:38 +0200

    components & routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProjectComponent } from './project/project.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomepageComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: ':title', component: ProjectComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { MenuComponent } from './menu/menu.component';
+import { ProjectComponent } from './project/project.component';
+import { GridComponent } from './grid/grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    MenuComponent,
+    ProjectComponent,
+    GridComponent
   ],
   imports: [
     BrowserModule,
