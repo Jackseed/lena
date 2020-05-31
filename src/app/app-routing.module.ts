c29c90b9bf8d867b14e5be1df7b995f5566d54a5
Jackseed <vinkho.c@gmail.com>
2020-05-31 19:03:48 +0200

    project page style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { ProjectPageComponent } from "./project-page/project-page.component";
 import { GridComponent } from "./grid/grid.component";
+import { ProjectComponent } from "./project/project.component";
 
 const routes: Routes = [
   { path: "home", component: GridComponent },
   { path: "", redirectTo: "/home", pathMatch: "full" },
-  { path: ":title", component: ProjectPageComponent },
+  { path: ":title", component: ProjectComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,20 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HomepageComponent } from "./homepage/homepage.component";
 import { MenuComponent } from "./menu/menu.component";
 import { ProjectComponent } from "./project/project.component";
 import { GridComponent } from "./grid/grid.component";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { MatGridListModule } from "@angular/material/grid-list";
-import { ProjectPageComponent } from './project-page/project-page.component';
-import { LayoutComponent } from './layout/layout.component';
+import { LayoutComponent } from "./layout/layout.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent,
     MenuComponent,
     ProjectComponent,
     GridComponent,
-    ProjectPageComponent,
     LayoutComponent,
   ],
   imports: [
