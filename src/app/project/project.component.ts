0c1394939e33236c0b556fd7eaa09f191a85ae08
Jackseed <vinkho.c@gmail.com>
2020-05-31 18:05:37 +0200

    project page

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Project, Projects } from "../models/project-list";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-project',
-  templateUrl: './project.component.html',
-  styleUrls: ['./project.component.scss']
+  selector: "app-project",
+  templateUrl: "./project.component.html",
+  styleUrls: ["./project.component.scss"],
 })
 export class ProjectComponent implements OnInit {
+  public project$: Observable<Project>;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.project$ = this.route.params.pipe(
+      map((p) => p.title),
+      map((title) =>
+        Projects.find((projet) => {
+          return projet.lien === title;
+        })
+      )
+    );
   }
-
 }
