2a62b289c95520a654f806df4543397a2ef3c5c6
Jackseed <vinkho.c@gmail.com>
2020-06-02 08:59:12 +0200

    delete an uploaded image

--- src/app/admin/upload-task/upload-task.component.ts
+++ src/app/admin/upload-task/upload-task.component.ts
@@ -14,12 +14,16 @@ import { finalize, tap } from "rxjs/operators";
 })
 export class UploadTaskComponent implements OnInit {
   @Input() file: File;
+  @Input() i: number;
 
   task: AngularFireUploadTask;
 
   percentage: Observable<number>;
   snapshot: Observable<any>;
   downloadURL: string;
+  path: string;
+  ref: any;
+  id: string;
 
   constructor(
     private storage: AngularFireStorage,
@@ -28,17 +32,18 @@ export class UploadTaskComponent implements OnInit {
 
   ngOnInit() {
     this.startUpload();
+    this.id = this.db.createId();
   }
 
   startUpload() {
     // The storage path
-    const path = `test/${Date.now()}_${this.file.name}`;
+    this.path = `test/${Date.now()}_${this.file.name}`;
 
     // Reference to storage bucket
-    const ref = this.storage.ref(path);
+    this.ref = this.storage.ref(this.path);
 
     // The main task
-    this.task = this.storage.upload(path, this.file);
+    this.task = this.storage.upload(this.path, this.file);
 
     // Progress monitoring
     this.percentage = this.task.percentageChanges();
@@ -47,13 +52,42 @@ export class UploadTaskComponent implements OnInit {
       tap(console.log),
       // The file's download URL
       finalize(async () => {
-        this.downloadURL = await ref.getDownloadURL().toPromise();
+        this.downloadURL = await this.ref.getDownloadURL().toPromise();
 
         this.db
           .collection("files")
-          .add({ downloadURL: this.downloadURL, path });
+          .doc(this.id)
+          .set({ downloadURL: this.downloadURL, path: this.path });
       })
     );
+  }
+  // TODO: use a function onDelete to delete the file on Storage
+  deleteImg() {
+    const imgRef = this.storage.storage.refFromURL(this.downloadURL);
+    // delete on firestore
+    this.db
+      .collection("files")
+      .doc(this.id)
+      .delete()
+      .then((_) => {
+        console.log("Image supprimée de la bdd !");
+        // delete on firestorage
+        imgRef
+          .delete()
+          .then((_) => {
+            console.log("Fichier supprimée de storage !");
+            this.downloadURL = "";
+          })
+          .catch((error) => {
+            console.error(
+              "Erreur dans la suppression fichier storage: ",
+              error
+            );
+          });
+      })
+      .catch((error) => {
+        console.error("Erreur dans la suppression bdd: ", error);
+      });
   }
 
   isActive(snapshot) {
