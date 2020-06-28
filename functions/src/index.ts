cc96b09b63579db88a61b0ce9bb5baf84916ddb0
Jackseed <vinkho.c@gmail.com>
2020-06-28 10:13:10 +0200

    function fixed

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,47 +1,52 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
-import { EventContext } from "firebase-functions";
+import { EventContext, Change } from "firebase-functions";
 import * as firebase from "firebase-admin";
 const db = admin.firestore();
 
 export const documentWriteListener = functions.firestore
   .document("projects/{projectId}/images/{imageId}")
-  .onCreate(
-    (snapshot: firebase.database.DataSnapshot, context: EventContext) => {
-      const projectId: string | null | undefined =
-        snapshot.ref.parent?.parent?.key;
-      const imageId: string | null = snapshot.ref.key;
-
-      if (imageId === null || projectId === null) {
-        return 0;
-      }
+  .onWrite(
+    (
+      change: Change<firebase.firestore.DocumentSnapshot>,
+      context: EventContext
+    ) => {
+      const projectId: string = context.params.projectId;
+      const imageId: string = context.params.imageId;
 
       const projectRef: firebase.firestore.DocumentReference = db
         .collection("projects")
         .doc(projectId);
+      if (!change.before.exists) {
+        // Update Customer
+        const imageRef = projectRef.collection("images").doc(imageId);
 
-      // Update Customer
-      const imageRef = projectRef.collection("images").doc(imageId);
+        return db.runTransaction(
+          async (transaction: firebase.firestore.Transaction) => {
+            const project = (await transaction.get(projectRef)).data();
 
-      return db.runTransaction(
-        async (transaction: firebase.firestore.Transaction) => {
-          const project = (await transaction.get(projectRef)).data();
+            let imageCount = project?.imageCount + 1;
 
-          const imageCount = project?.imageCount + 1;
+            if(!imageCount) {
+              imageCount = 0;
+            }
 
-          transaction.update(projectRef, {
-            imageCount,
-          });
+            transaction.update(projectRef, {
+              imageCount,
+            });
 
-          transaction.set(
-            imageRef,
-            {
-              position: imageCount,
-            },
-            { merge: true }
-          );
-        }
-      );
+            transaction.set(
+              imageRef,
+              {
+                position: imageCount,
+              },
+              { merge: true }
+            );
+          }
+        );
+      } else {
+        return 0;
+      }
     }
   );
